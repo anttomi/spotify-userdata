328b67e25915f4fceeca02eea324693dda892162
anttomi <tomi.salo@myy.haaga-helia.fi>
2021-12-04 18:25:29 +0200

    optimized formArtists function

--- spotify.py
+++ spotify.py
@@ -1,16 +1,13 @@
 import json, datetime, operator, os
 import tkinter as tk
 from io import open
+from functools import reduce
 
 class Artist:
     def __init__(self, name):
         self.name = name
-        self.count = 0
         self.duration = 0
         self.tracks = {}
-
-    def addCount(self):
-        self.count += 1
 
     def addDuration(self, duration):
         self.duration += duration
@@ -25,16 +22,17 @@ class Artist:
         self.tracks[track] += 1
 
     def getSortedList(self):
-        songs = self.tracks
-        return sorted(songs.items(), key=lambda x: int(x[1]), reverse=True)
+        return sorted(self.tracks.items(), key=lambda x: int(x[1]), reverse=True)
     
     def getMostPlayedSong(self):
-        songs = self.tracks
-        songs = sorted(songs.items(), key=lambda x: int(x[1]), reverse=True)
-        return songs[0]
+        return sorted(self.tracks.items(), key=lambda x: int(x[1]), reverse=True)[0]
+    
+    def getPlayCount(self):
+        return reduce(lambda a, b: a+b, self.tracks.values())
 
     def __str__(self):
-        return "\n%s \nTimes Played: %s \nDuration: " % (self.name, self.count) + '{:.2f}'.format((self.duration/1000)/60/60) + "\nMost Played Song: " + str(self.getMostPlayedSong()) +"\n"
+        return "\n%s \nTimes Played: %s \nDuration: " % (self.name, self.getPlayCount()) + '{:.2f}'.format((self.duration/1000)/60/60) + "\nMost Played Song: " + str(self.getMostPlayedSong()) +"\n"
+
 
 #Artist dictionary printing method
 def outputDictionary(artists):
@@ -49,20 +47,17 @@ def outputDictionary(artists):
 
 #Forming the artists dictionary and collecting data
 def formArtists(jsons, threshold=5000):
-    #Temp artist names list for easier structure in main
-    artistNames = []
     #Artist dictionary that contains artist objects
     artists = {}
     #How many ms to have been streamed a song to be counted
     countAsListen = threshold
-    #Total duration of streamed songs, which were streamed more than countAsListen threshold variable
+    #Total duration of streamed songs, which were streamed longer than countAsListen threshold variable
     duration = 0
     #Keeping count of jsons
     i = 0
-    #line = 0
     while (i < jsons):
-        print("%d/%d" % (i, jsons-1))
-        #Open the streaming history file from the script directory, encode it so cyrillic alphabets don't cause problems
+        print("%d/%d" % (i+1, jsons))
+        #Open the streaming history file from the script directory, encoding so cyrillic alphabets don't cause problems
         with open(os.path.dirname(__file__) + './endsong_%d.json' % (i), 'r', encoding="utf-8") as streamsJson:
             streams = json.load(streamsJson)
             if i == 0:
@@ -81,30 +76,20 @@ def formArtists(jsons, threshold=5000):
                 songDuration = song["ms_played"]
                 trackName = song["master_metadata_track_name"]
                 #if artist not created, then create an artist object and a first stream
-                if artistName not in artistNames:
-                    artistNames.append(artistName)
+                if artistName not in artists.keys():
                     artists[artistName] = Artist(artistName)
-                    if songDuration > countAsListen:
-                        artists[artistName].addCount()
-                        artists[artistName].addDuration(songDuration)
-                else:
-                    #if artist already created, add a playcount to it if song duration exceeds the threshold
-                    if songDuration > countAsListen:
-                        artists[artistName].addCount()
-                        artists[artistName].addDuration(songDuration)
 
-                if trackName not in artists[artistName].tracks:
+                if trackName not in artists[artistName].tracks.keys():
                     artists[artistName].addSong(trackName)
                 else:
                     try:
                         if songDuration > countAsListen:
                             artists[artistName].addSongCount(trackName)
+                            artists[artistName].addDuration(songDuration)
                     except KeyError:
                         print(trackName + " KeyError")
                 duration += songDuration
-                #line += 1
             i += 1
-            #line = 0
     return artists,start,end,duration
 
 def timeformat(start, end):
@@ -120,7 +105,7 @@ def guiMain():
         artists,start,end,duration = formArtists(jsons)
     else:
         artists,start,end,duration = formArtists(jsons, threshold)
-        
+
     window = tk.Tk()
     timedelta = timeformat(start, end)
     label = tk.Label(text="Streams are between %s hours (%s - %s)\nHours streamed: " % (timedelta, start, end) +
@@ -133,8 +118,4 @@ def guiMain():
         textbox.insert(1.0, artist)
     tk.mainloop()
 
-
-
 guiMain()
-
-
